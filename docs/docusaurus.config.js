47be9a198c9e76426efdc1a439a46e61d1581ee8
Jamie Rees <Jamie.Rees@exclaimer.com>
2023-07-12 09:24:28 +0100

    docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -85,8 +85,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Gettting Started',
+                to: '/',
               },
             ],
           },
